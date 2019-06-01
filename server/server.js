6da13d1fa37c794cb2025f7817785bab25bcf485
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-05-31 21:52:52 -0400

    fix server to serve index.html

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-const app = require('./api/app');
+const app = require('./app');
 let port = process.env.PORT || 3000;
 
+//Start server
 app.listen(port, () => console.log(`Server listening on: http://localhost:3000`));
