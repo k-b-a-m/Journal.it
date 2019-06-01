5971a84509a016c96c61f5afc30410d9efe381b6
Alex Waite <alexcwaite@gmail.com>
2019-06-01 13:21:43 -0400

    moved redux store

---
+++ client/redux/store.js
@@ -0,0 +1,50 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import axios from 'axios';
+
+//ACTION TYPES
+
+const SET_ENTRIES = 'SET_ENTRIES';
+
+//ACTION CREATORS
+
+const setEntries = entries => ({
+  type: SET_ENTRIES,
+  entries,
+});
+
+//THUNK CREATORS
+
+export const fetchEntries = () => async dispatch => {
+  try {
+    const response = await axios.get('/entries');
+    const entries = response.data;
+    return dispatch(setEntries(entries));
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export const addEntryThunk = entry => {
+  return dispatch => {
+    return axios
+      .post(`/entries`, entry)
+      .then(() => dispatch(fetchEntries()))
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+};
+
+//REDUCER
+
+const entries = (state = [], action) => {
+  switch (action.type) {
+    case SET_ENTRIES:
+      return action.entries;
+    default:
+      return state;
+  }
+};
+
+export const store = createStore(entries, applyMiddleware(thunkMiddleware));
