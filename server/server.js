dc28add561de322b562be2fcb37b88932afe0811
Alex Waite <alexcwaite@gmail.com>
2019-06-01 13:32:35 -0400

    added syncAndSeed, data to server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const app = require('./app');
 let port = process.env.PORT || 3000;
+const { syncAndSeed } = require('./db');
 
 //Start server
-app.listen(port, () => console.log(`Server listening on: http://localhost:3000`));
+app.listen(port, () =>
+  console.log(`Server listening on: http://localhost:3000`)
+);
+
+syncAndSeed();
