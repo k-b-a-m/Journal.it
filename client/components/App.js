9555b418bd72032ffa6afffc8891ed8448b467c3
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-06-02 20:49:04 -0400

    staged 2

--- client/components/App.js
+++ client/components/App.js
@@ -3,14 +3,15 @@ import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import GoogleApiWrapper from './GoogleMaps';
 import axios from 'axios';
+import {connect} from 'react-redux';
 
 //components
-import Home from "./Home";
-import Entry from "./Entry";
-import Nav from "./Nav";
+import Home from './Home';
+import Entry from './Entry';
+import Nav from './Nav';
 
 //styles
-import "../styles/App.css";
+import '../styles/App.css';
 
 class App extends Component {
   constructor() {
@@ -27,14 +28,16 @@ class App extends Component {
         <Nav />
         <Route exact path="/" component={Home} />
         <Route path="/entry" component={Entry} />
-{/*
+        {/*
         <Route
           path="/summary/:word"
           render={props => <Summary props={props} entries={entries} />}
         /> */}
         <Route
           path="/map"
-          render={props => <GoogleApiWrapper props={props} /*entries={entries}*/ />}
+          render={props => (
+            <GoogleApiWrapper props={props} /*entries={entries}*/ />
+          )}
         />
       </div>
     );
@@ -43,5 +46,5 @@ class App extends Component {
 
 export default connect(
   null,
-  { fetchEntries }
+  {fetchEntries}
 )(App);
