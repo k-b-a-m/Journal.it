ce5684e90da8e9ec5b51b47348857b8646c4f856
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-06-02 23:12:18 -0400

    testing out light

--- client/components/Home2.js
+++ client/components/Home2.js
@@ -8,35 +8,58 @@ class Home2 extends Component {
     super(props);
   }
   componentDidMount() {
-    const width = this.mount.clientWidth;
-    const height = this.mount.clientHeight;
+    // const width = this.mount.clientWidth;
+    // const height = this.mount.clientHeight;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
+    //setting scene, camera, renderer to render three js
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 2000);
     const renderer = new THREE.WebGLRenderer({antialias: true});
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({color: '#433F81'});
-    const cube = new THREE.Mesh(geometry, material);
-
-    camera.position.z = 4;
-    scene.add(cube);
-    renderer.setClearColor('#000000');
+    renderer.setClearColor(0x00ff00);
     renderer.setSize(width, height);
 
+    const light = new THREE.AmbientLight(0xffffff,0.5);
+    scene.add(light);
+    const light1 = new THREE.PointLight(0xffffff,0.5);
+    scene.add(light1);
+
+    //rendering sphere
+    // const geometry = new THREE.SphereGeometry(10, 32, 32);
+    // const material = new THREE.MeshBasicMaterial({color: 'green'});
+    // const sphere = new THREE.Mesh(geometry, material);
+    // scene.add(sphere);
+    // camera.position.z = 3;
+
+    //rendering cube
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshLambertMaterial({color: 0xffff00});
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    //move camera out to view cube
+    camera.position.z = 4;
+
+    //bind element to renderer
     this.scene = scene;
     this.camera = camera;
     this.renderer = renderer;
     this.material = material;
     this.cube = cube;
+    this.light = light;
+    this.light1 = light1;
+    // this.sphere = sphere;
 
     this.mount.appendChild(this.renderer.domElement);
     this.start();
   }
+
   componentWillUnmount() {
     this.stop();
     this.mount.removeChild(this.renderer.domElement);
   }
 
+  //start and stop will grab animation frame for animation at 60fps
   start = () => {
     if (!this.frameId) {
       this.frameId = requestAnimationFrame(this.animate);
