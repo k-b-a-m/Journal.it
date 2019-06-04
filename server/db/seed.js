c10c0e3ab880ea6013b2cb1b00a1637a2867e42a
Kyle Jiang <keeler@gmail.com>
2019-06-03 23:16:03 -0400

    added ~5000 fake entries around nyc for heatmap work

--- server/db/seed.js
+++ server/db/seed.js
@@ -3,15 +3,22 @@ const faker = require("faker");
 const makeFakeEntry = () => {
   return {
     content: faker.lorem.sentence(),
-    latitude: faker.address.latitude(),
-    longitude: faker.address.longitude(),
+    //Faking loads of fake data around NYC
+    latitude: faker.address.latitude({
+      min:40.6,
+      max:40.8
+    }),
+    longitude: faker.address.longitude({
+      min:73.9,
+      max:74.1
+    }),
     likes: faker.random.number()
   };
 };
 
 const entries = [];
 
-for (let i = 0; i < 50; i++) {
+for (let i = 0; i < 5000; i++) {
   entries.push(makeFakeEntry());
 }
 
