7e308842c120b0bcdc77095ee5f5968f07598bf2
Kyle Jiang <keeler@gmail.com>
2019-06-05 12:57:33 -0400

    slight change to app

--- server/server.js
+++ server/server.js
@@ -1,10 +1,34 @@
-const app = require('./app');
+const app = require("./app");
+const fs = require("fs");
+const https = require("https");
+const http = require("http");
+const path = require("path");
 let port = process.env.PORT || 3000;
-const { syncAndSeed } = require('./db');
+let portSecure = process.env.PORT || 8443;
+const { syncAndSeed } = require("./db");
 
-//Start server
-app.listen(port, () =>
-  console.log(`Server listening on: https://localhost:3000`)
-);
+//HTTP Server
+http
+  .createServer(app)
+  .listen(port, () =>
+    console.log(`HTTP Server listening on: https://localhost:${port}`)
+  );
+
+//HTTPS Server
+https
+  .createServer(
+    {
+      key: fs.readFileSync(
+        path.join(__dirname, "..", "encryption", "server.key")
+      ),
+      cert: fs.readFileSync(
+        path.join(__dirname, "..", "encryption", "server.cert")
+      )
+    },
+    app
+  )
+  .listen(portSecure, () =>
+    console.log(`HTTPS Server listening on: https://localhost:${portSecure}`)
+  );
 
 syncAndSeed();
