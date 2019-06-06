c5058d1de9595033bf1844632492ff890da8b213
Alex Waite <alexcwaite@gmail.com>
2019-06-06 16:41:23 -0400

    refactor: include ReactDependentScript wrapper on Map component to load google maps api script properly

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,12 @@
 //libraries
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import GoogleApiWrapper from './GoogleMaps';
 import { connect } from 'react-redux';
+import ReactDependentScript from 'react-dependent-script';
 
 //components
 import HomeSphere from './Home-Sphere';
 import Home from './Home';
-import Entry from './Entry';
 import Nav from './Nav';
 import Map from './Map';
 
@@ -18,16 +17,11 @@ import { fetchNearby } from '../redux/store';
 import '../styles/App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
     var socket = io('https://localhost:8443');
     socket.on('updateNearby', entry => {
       console.log(entry);
     });
-
 
     navigator.geolocation.getCurrentPosition(position => {
       const { latitude, longitude } = position.coords;
@@ -37,14 +31,6 @@ class App extends Component {
           console.log(resp.entries);
         });
     });
-
-    const googleMapsScript = document.createElement('script');
-    googleMapsScript.type = 'text/javascript';
-    googleMapsScript.defer = true;
-    googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=${
-      process.env.GOOGLE_API_KEY
-    }&libraries=visualization`;
-    document.getElementsByTagName('head')[0].appendChild(googleMapsScript);
   }
 
   render() {
@@ -54,12 +40,21 @@ class App extends Component {
         <Route exact path="/" component={Home} />
         <Route exact path="/homesphere" component={HomeSphere} />
         {/* <Route path="/entry" component={Entry} /> */}
-        {/*
         <Route
-          path="/summary/:word"
-          render={props => <Summary props={props} entries={entries} />}
-        /> */}
-        <Route exact path="/map" component={Map} />
+          exact
+          path="/map"
+          render={() => (
+            <ReactDependentScript
+              scripts={[
+                `https://maps.googleapis.com/maps/api/js?key=${
+                  process.env.GOOGLE_API_KEY
+                }&libraries=visualization`,
+              ]}
+            >
+              <Map />
+            </ReactDependentScript>
+          )}
+        />
       </div>
     );
   }
