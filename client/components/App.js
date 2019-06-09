e65e1a71a8db45130de1204c6c668720384ed28e
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-06-09 13:15:31 -0400

    fix store reducers, app state

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,8 @@
 //libraries
-<<<<<<< HEAD
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
-=======
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
 import axios from 'axios';
->>>>>>> ca06213fb54c8ad4b750b8efc80b94b9333304d9
 import ReactDependentScript from 'react-dependent-script';
 //import {GOOGLE_API_KEY} from '../../config';
 
@@ -25,32 +19,33 @@ import {fetchNearby} from '../redux/store';
 import '../styles/App.css';
 
 class App extends Component {
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {key: ''};
+  }
+
+  async componentDidMount() {
     var socket = io('http://localhost:3000');
     socket.on('updateNearby', entry => {
       console.log(entry);
     });
 
     navigator.geolocation.getCurrentPosition(position => {
-<<<<<<< HEAD
       const {latitude, longitude} = position.coords;
       this.props.fetchNearby({
         coordinate: {latitude, longitude},
-=======
-      const { latitude, longitude } = position.coords;
-      this.props.fetchNearby({
-        coordinate: { latitude, longitude },
->>>>>>> ca06213fb54c8ad4b750b8efc80b94b9333304d9
         distance: 500,
       }); //distance is in feet 5280ft = 1mi
     });
 
     await axios
       .get('/googlemaps')
-      .then(response => this.setState({ key: response.data }));
+      .then(response => this.setState({key: response.data}));
+    console.log('hi');
   }
 
   render() {
+    console.log(this.state.key);
     return this.state.key.length ? (
       <div>
         {/* <Nav /> */}
