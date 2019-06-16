1a1a113752af17f71367253cc4687481d31f3060
Marfuen <marfuen98@gmail.com>
2019-06-16 11:39:32 -0400

    Users

--- client/components/UserPage.js
+++ client/components/UserPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Col, Row, Container, ListGroup} from 'react-bootstrap';
+import {Col, Row, Container, ListGroup, Card} from 'react-bootstrap';
 import Nav from './Nav';
 import {fetchUser} from '../redux/store';
 import {connect} from 'react-redux';
@@ -17,24 +17,40 @@ class UserProfile extends Component {
   }
   render(){
     const {user} = this.state;
-    console.log(JSON.stringify(user))
+    console.log(user.user);
     return(
       <div style={{backgroundColor: 'black', color: 'white'}}>
         <Container>
           <Row>
             <Col>
+              <h5>User Profile Page</h5>
               {user.user ?
-                <ul >
-                  {user.user.entries.map(entry =>
-                    <li key={entry.id}>
-                      {entry.latitude}
-                    </li>
-                  )}
-                </ul>
+              <div>
+                <Card style={{color: 'black'}} className="mb-3">
+                  <Card.Body>
+                    <Card.Text>
+                      Hello {user.user.name}!
+                    </Card.Text>
+                    <hr/>
+                    <Card.Text>
+                      Here are all your stories!
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+                {user.user.entries.length > 0
+                }
+                {user.user.entries.map(entry =>
+                  <Card key={entry.id} style={{color: 'black'}} className="mb-3">
+                    <Card.Body>
+                      <Card.Text>
+                        {entry.content}
+                      </Card.Text>
+                      <button className="btn btn-warning">Renew Entry</button>
+                    </Card.Body>
+                  </Card>
+                )}
+              </div>
               : null}
-            </Col>
-            <Col>
-              <h1>Welcome to the user page 2</h1>
             </Col>
           </Row>
         </Container>
