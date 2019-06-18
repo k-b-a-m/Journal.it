1555baa24b7198c67d3e00366dcd0d05222015b1
Alex Waite <alexcwaite@gmail.com>
2019-06-17 21:26:52 -0400

    resolved style conflict between UserPage and Nav

--- client/components/App.js
+++ client/components/App.js
@@ -41,6 +41,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Nav />
         <Route exact path="/" component={Home} />
         <Route path="/homecheck" component={HomeCheck} />
         <Route
